c9ba0bf5bfd1f893320c26353f9487b7b9b946b6
Danil-Sharipov <121438053+Danil-Sharipov@users.noreply.github.com>
2023-12-05 22:56:12 +0300

    just for Danial

--- dags/test.py
+++ dags/test.py
@@ -1,56 +1,57 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.s3_to_local_operator import S3ToLocaFileSystemOperator
+from airflow.operators.bash_operator import BashOperator
 
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
-# [START tutorial]
-# [START import_module]
-import json
-
-import pendulum
-
-from airflow.decorators import dag, task
-@dag(
-    schedule_interval=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    catchup=False,
-    tags=['example'],
+dag = DAG(
+    'seismic_data_dag',
+    default_args=default_args,
+    description='DAG for downloading seismic data from S3',
+    schedule_interval=timedelta(days=1),
 )
-def JUST_TEST():
-    @task()
-    def extract():
-        hook = S3Hook(aws_conn_id="S3_ETL_CONN")
-        extra = hook.get_connection(hook.aws_conn_id).get_extra()
-        source_s3.download(source_s3_key,source_s3_bucket,dest_file_path)
-    @task(multiple_outputs=True)
-    def transform(order_data_dict: dict):
-        total_order_value = 0
 
-        for value in order_data_dict.values():
-            total_order_value += value
+# S3 bucket and prefix where seismic data is stored
+s3_bucket = 'my-test-bucket'
+s3_prefix = 'your-s3-prefix'
 
-        return {"total_order_value": total_order_value}
-    @task()
-    def load(total_order_value: float):
+# Local directory to store downloaded files
+local_dir = '/path/to/local/directory'
 
-        print(f"Total order value is: {total_order_value:.2f}")
+# Download 12 files
+for i in range(1, 13):
+    task_id = f'download_file_{i}'
+    s3_key = f'{s3_prefix}/seismic_data_{i}.csv'
 
-    order_data = extract()
-    order_summary = transform(order_data)
-    load(order_summary["total_order_value"])
+    # Download file from S3 to local filesystem
+    download_task = S3ToLocaFileSystemOperator(
+        task_id=task_id,
+        aws_conn_id='aws_default',
+        bucket_name=s3_bucket,
+        object_name=s3_key,
+        local_file=local_dir,
+        replace=True,
+        dag=dag,
+    )
 
-tutorial_etl_dag = tutorial_taskflow_api_etl()
+    # You can perform any additional processing here if needed
+
+    # Bash operator to print a message indicating successful download
+    success_message = f"echo 'Seismic data file {i} downloaded successfully'"
+    success_task = BashOperator(
+        task_id=f'success_message_{i}',
+        bash_command=success_message,
+        dag=dag,
+    )
+
+    # Set up dependencies between tasks
+    download_task >> success_task
+
+# You can define additional tasks and dependencies as needed
--- load-in-minio.py
+++ load-in-minio.py
@@ -1,10 +1,11 @@
 import boto3
-
+from tqdm import tqdm
 # Инициализация клиента S3
+ackey,seckey = input().split()
 s3_client = boto3.client('s3',
                          endpoint_url='http://localhost:9000', 
-                         aws_access_key_id='nWkSrEchrTAWItY4iAi7',
-                         aws_secret_access_key='58KPiyb5FzqUb1euaEIFfTZ2caSRYZGwVvjxNK42')
+                         aws_access_key_id=ackey,
+                         aws_secret_access_key=seckey)
 
 # Создание бакета
 bucket_name = 'my-test-bucket'
@@ -19,5 +20,5 @@ from os import listdir
 from os.path import isfile, join
 mypath = './data/01'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-for i in onlyfiles:
+for i in tqdm(onlyfiles):
     s3_client.upload_file(mypath+'/'+i,bucket_name,i)
